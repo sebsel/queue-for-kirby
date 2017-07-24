4c62ba01af48c97eb9875db8d791fbd0eb7d9d75
sebsel <sebsel@seblog.nl>
2017-07-24 02:30:44 +0200

    need to load $site too

--- worker.php
+++ worker.php
@@ -4,6 +4,7 @@ require(dirname(__DIR__) . DIRECTORY_SEPARATOR .
     'kirby' . DIRECTORY_SEPARATOR . 'bootstrap.php');
 
 $kirby = kirby();
+$site  = site();
 
 $kirby->extensions();
 $kirby->models();
