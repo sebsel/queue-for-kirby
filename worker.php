df0209e59c037b594be52575c7738f108b7c7a4b
sebsel <sebsel@seblog.nl>
2017-07-24 17:11:26 +0200

    don't load all of Kirby when there are no jobs
    Again, thanks @Zegnat

--- worker.php
+++ worker.php
@@ -4,6 +4,10 @@ require(dirname(__DIR__) . DIRECTORY_SEPARATOR .
     'kirby' . DIRECTORY_SEPARATOR . 'bootstrap.php');
 
 $kirby = kirby();
+$kirby->plugin('queue-for-kirby');
+
+if (!queue::hasJobs()) exit();
+
 $site  = site();
 
 $kirby->extensions();
